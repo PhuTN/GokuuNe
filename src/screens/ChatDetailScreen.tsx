df87e331a8e1b78f3027be4ed2321515e4542f5e
Present111 <161811020+Present111@users.noreply.github.com>
2025-04-01 21:43:56 +0700

    Full

--- src/screens/ChatDetailScreen.tsx
+++ src/screens/ChatDetailScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, FlatList, StyleSheet} from 'react-native';
+import {View, FlatList, StyleSheet, Text} from 'react-native';
 import ChatDetailHeader from '../components/common/ChatDetailScreen/ChatDetailHeader';
 import MessageBubble from '../components/common/ChatDetailScreen/MessageBubble';
 import MessageInput from '../components/common/ChatDetailScreen/MessageInput';
@@ -24,6 +24,10 @@ const ChatDetailScreen = () => {
   return (
     <View style={styles.container}>
       <ChatDetailHeader />
+      {/* Add "Today" label below the header */}
+      <View style={styles.todayLabelContainer}>
+        <Text style={styles.todayLabelText}>Today</Text>
+      </View>
       <FlatList
         data={messages}
         keyExtractor={item => item.id}
@@ -44,6 +48,13 @@ const ChatDetailScreen = () => {
 const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: '#fff'},
   messages: {paddingTop: 10, paddingBottom: 60}, // Add space for input
+  todayLabelContainer: {
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  todayLabelText: {
+    fontSize: 22,
+  },
 });
 
 export default ChatDetailScreen;
