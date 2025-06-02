cbe1c8beaa42f5f23cce961b064f6971fec2763e
BinhUIT <22520128@gm.uit.edu.vn>
2025-06-02 18:44:37 +0700

    Cap nhat lai endpoint server

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: 'https://be8b-113-161-91-9.ngrok-free.app',    // 👈 đổi url của bạn ở đây
+    API_URL: 'https://ca98-113-161-91-48.ngrok-free.app',    // 👈 đổi url của bạn ở đây
     TIMEOUT: 10000                         // timeout 10s
 };
 
